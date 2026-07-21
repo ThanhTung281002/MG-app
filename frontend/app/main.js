3060229537c75c85bd2ba0a7064b82191deb955e
ThanhTung281002 <tung0905203565@gmail.com>
2026-07-21 15:20:07 +0700

    fix: chuyển thời gian tự động lưu từ 1.5s --> 2.5s -> giải quyết sự bất tiện khi người dùng đang viết và dừng lại một chút để suy nghĩ thì app tự động lưu mất

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -3644,7 +3644,7 @@ async function handleAaMiniAction(miniAction, content) {
 
 // hàm xử lí khi nhập input 
 let saveTimer = null; 
-const saveTime = 1500; // 5s
+const saveTime = 2500; // 2.5s
 
 
 async function handleInput(contentType, content) {
